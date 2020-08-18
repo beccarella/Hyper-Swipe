ce597a7a9c01466f5475c9e143cc1444a01f40fa
beccarella <urquhart.becca@gmail.com>
2020-08-18 12:08:59 +0200

    Enabled clicking through all the cards

--- src/components/SwiperCard.js
+++ src/components/SwiperCard.js
@@ -5,19 +5,37 @@ const url = 'https://hyper-swipe.herokuapp.com/cards';
 
 const SwiperCard = () => {
   const [cardData, setCardData] = useState([]);
+  const [loading, setLoading] = useState(false)
+  const [likedItem, setLikedItem] = useState([]);
+  const [dislikedItem, setDislikedItem] = useState([]);
 
   useEffect(() => {
+    setLoading(true)
     fetch(url)
       .then(res => res.json())
-      .then(cardData => setCardData(cardData))
-  });
+      .then(cardData => {setCardData(cardData); setLoading(false)})
+  },[]);
+
+  const removeCard = id => {
+    setCardData(cardData.filter(item => item.id !== id));
+  };
+  
+  const handleLike = (itemId, itemImg, itemTitle) => {
+    setLikedItem([...likedItem, {itemId, itemImg, itemTitle}]);
+    removeCard(itemId);
+  };
+
+  const handleDislike = (itemId, itemImg, itemTitle) => {
+    setDislikedItem([...dislikedItem, {itemId, itemImg, itemTitle}]);
+    removeCard(itemId);
+  };
 
   const cardItems = cardData.map((item, index) => {
     return(
       <Card key={index} className="cardContainer">
         <Container className="btnContainer">
           <div className="btnWrapper">
-            <Button className="btn" onClick={() => console.log(item.id)}>DISLIKE</Button>
+            <Button className="btn" onClick={() => handleDislike(item.id, item.image, item.title)}>DISLIKE</Button>
           </div>
         </Container>
         <Container className="cardContentContainer">
@@ -33,7 +51,7 @@ const SwiperCard = () => {
         </Container>
         <Container className="btnContainer">
           <div className="btnWrapper">
-            <Button className="btn" onClick={() => console.log(item.id)}>LIKE</Button>
+            <Button className="btn" onClick={() => handleLike(item.id, item.image, item.title)}>LIKE</Button>
           </div>
         </Container>
       </Card>
