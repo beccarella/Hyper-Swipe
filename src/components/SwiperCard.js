7bd8e893a2a42c77eb3c263ef1c2248989a1d772
beccarella <urquhart.becca@gmail.com>
2020-08-24 00:30:39 +0200

    Changed how data is written to localStorage. Persistence still not working.

--- src/components/SwiperCard.js
+++ src/components/SwiperCard.js
@@ -7,6 +7,15 @@ const SwiperCard = () => {
   const [cardData, setCardData] = useState([]);
   const [, setLoading] = useState(false);
 
+  const [clicked, setClicked] = useState(() => {
+    const localData = localStorage.getItem('clicked');
+    return localData ? JSON.parse(localData) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('clicked', JSON.stringify(clicked))
+  }, [clicked])
+
   useEffect(() => {
     setLoading(true)
     fetch(url)
@@ -20,7 +29,7 @@ const SwiperCard = () => {
   
   const handleBtnClick = (itemId, index) => {
     let clickedCard = cardData[index];
-    localStorage.setItem(itemId, JSON.stringify(clickedCard));
+    setClicked([...clicked, JSON.stringify(clickedCard)]);
     removeCard(itemId);
   };
 
@@ -56,6 +65,20 @@ const SwiperCard = () => {
           })) : 
           <div>
             <h2>There are no more cards.</h2>
+            {/* <div>
+              {localStorage.getItem('clicked') !== null ? 
+                clicked.map(card => {                  
+                  return(
+                    <Card key={card.id} className="likedCard">
+                      <Card.Img alt="item you liked" src={card.image}/>
+                      <Card.Body>
+                        <Card.Title>{card.title}</Card.Title>
+                      </Card.Body>
+                    </Card>
+                  ) 
+                }) : ([])
+              }
+            </div> */}
           </div>
       }
     </div>
